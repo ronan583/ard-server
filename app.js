8c64be7070214e5dfb9583889c4a3bd84523f32e
Research Design - Admin <rnd.admin@WMS.local>
2023-11-23 17:22:37 +1300

    modify

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const clients = new Set();
 wss.on('connection', function connection(ws) {
   clients.add(ws);
   ws.on('message', function incoming(message) {
+    console.log(message);
     clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log(message);
         client.send(message);
       }
     });
