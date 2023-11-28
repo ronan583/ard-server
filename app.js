f28e2395bb634d624c135554f4f90a727e6041b8
Research Design - Admin <rnd.admin@WMS.local>
2023-11-29 11:12:36 +1300

    postgres

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8765;
 
 // app.use(express.static.apply("public"));
 const serverPort = PORT;
 const server = http.createServer(app);
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
+const fs = require("fs").promises;
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const MilkingSessionManager = require("./milkingSession.js");
 
 const wss = new WebSocket.Server({ server });
 
@@ -15,28 +18,226 @@ server.listen(serverPort);
 
 const clients = new Set();
 
-app.get('/', (req, res) => {
+const MilkingStorage = require("./milkingStorage.js");
+const storage = new MilkingStorage();
+
+app.get("/", (req, res) => {
   res.write(`<h1>Socket IO Start on Port : ${PORT}</h1>`);
   res.end();
 });
 
-wss.on('connection', function connection(ws) {
+let isMilkingActive = false;
+let milkingName = "";
+let onMilking = false;
+
+const EMPTY_VALUE = -1;
+
+wss.on("connection", function connection(ws) {
   clients.add(ws);
   console.log("Connected!");
-  ws.send('Welcome to the WebSocket server!');
-  ws.on('message', function incoming(message) {
-    console.log(message);
-    clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+  ws.send("Welcome to the WebSocket server!");
+  ws.on("message", async (data) => {
+    console.log("Received data", String(data));
+    // clients.forEach((client) => {
+    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
+    //     client.send(data);
+    //   }
+    // });
+    if (!isMilkingActive) {
+      isMilkingActive = true;
+      milkingName = createMilkingFileName();
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      const now = new Date();
+      const formattedDate = `${now.getDate()}/${
+        now.getMonth() + 1
+      }/${now.getFullYear()}`;
+      const formattedTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+      const adc = jsonData.adc;
+      const rgbArr = [];
+      const sens = jsonData.Sens;
+      console.log("Sens: ", sens);
+      for (let i = 0; i < 4; i++) {
+        if (adc[i] == 1 && sens[i].d) {
+          rgbArr.push(sens[i].d.X);
+          rgbArr.push(sens[i].d.Y);
+          rgbArr.push(sens[i].d.Z);
+          rgbArr.push(sens[i].d.I1);
+          rgbArr.push(sens[i].d.I2);
+        } else {
+          rgbArr.push(null);
+          rgbArr.push(null);
+          rgbArr.push(null);
+          rgbArr.push(null);
+          rgbArr.push(null);
+        }
       }
-    });
+      let dataLine = null;
+      dataLine = {
+        name: milkingName,
+        date: formattedDate,
+        time: formattedTime,
+        adc0: jsonData.adc[0] * 1,
+        adc1: jsonData.adc[1] * 1,
+        adc2: jsonData.adc[2] * 1,
+        adc3: jsonData.adc[3] * 1,
+        r_br:
+          adc[0] == 1 && jsonData.Sens[0].d
+            ? jsonData.Sens[0].d.X
+            : EMPTY_VALUE,
+        g_br:
+          adc[0] == 1 && jsonData.Sens[0].d
+            ? jsonData.Sens[0].d.Y
+            : EMPTY_VALUE,
+        b_br:
+          adc[0] == 1 && jsonData.Sens[0].d
+            ? jsonData.Sens[0].d.Z
+            : EMPTY_VALUE,
+        ir1_br:
+          adc[0] == 1 && jsonData.Sens[0].d
+            ? jsonData.Sens[0].d.I1
+            : EMPTY_VALUE,
+        ir2_br:
+          adc[0] == 1 && jsonData.Sens[0].d
+            ? jsonData.Sens[0].d.I2
+            : EMPTY_VALUE,
+        r_bl:
+          adc[1] == 1 && jsonData.Sens[1].d
+            ? jsonData.Sens[1].d.X
+            : EMPTY_VALUE,
+        g_bl:
+          adc[1] == 1 && jsonData.Sens[1].d
+            ? jsonData.Sens[1].d.Y
+            : EMPTY_VALUE,
+        b_bl:
+          adc[1] == 1 && jsonData.Sens[1].d
+            ? jsonData.Sens[1].d.Z
+            : EMPTY_VALUE,
+        ir1_bl:
+          adc[1] == 1 && jsonData.Sens[1].d
+            ? jsonData.Sens[1].d.I1
+            : EMPTY_VALUE,
+        ir2_bl:
+          adc[1] == 1 && jsonData.Sens[1].d
+            ? jsonData.Sens[1].d.I2
+            : EMPTY_VALUE,
+        r_fr:
+          adc[2] == 1 && jsonData.Sens[2].d
+            ? jsonData.Sens[2].d.X
+            : EMPTY_VALUE,
+        g_fr:
+          adc[2] == 1 && jsonData.Sens[2].d
+            ? jsonData.Sens[2].d.Y
+            : EMPTY_VALUE,
+        b_fr:
+          adc[2] == 1 && jsonData.Sens[2].d
+            ? jsonData.Sens[2].d.Z
+            : EMPTY_VALUE,
+        ir1_fr:
+          adc[2] == 1 && jsonData.Sens[2].d
+            ? jsonData.Sens[2].d.I1
+            : EMPTY_VALUE,
+        ir2_fr:
+          adc[2] == 1 && jsonData.Sens[2].d
+            ? jsonData.Sens[2].d.I2
+            : EMPTY_VALUE,
+        r_fl:
+          adc[3] == 1 && jsonData.Sens[3].d
+            ? jsonData.Sens[3].d.X
+            : EMPTY_VALUE,
+        g_fl:
+          adc[3] == 1 && jsonData.Sens[3].d
+            ? jsonData.Sens[3].d.Y
+            : EMPTY_VALUE,
+        b_fl:
+          adc[3] == 1 && jsonData.Sens[3].d
+            ? jsonData.Sens[3].d.Z
+            : EMPTY_VALUE,
+        ir1_fl:
+          adc[3] == 1 && jsonData.Sens[3].d
+            ? jsonData.Sens[3].d.I1
+            : EMPTY_VALUE,
+        ir2_fl:
+          adc[3] == 1 && jsonData.Sens[3].d
+            ? jsonData.Sens[3].d.I2
+            : EMPTY_VALUE,
+      };
+
+      // let message;
+      // if (jsonData.adc == "1111" && !onMilking) {
+      //   message = "Milking started, all 4 cups working.";
+      //   console.log(message);
+      //   onMilking = true;
+      // } else if (jsonData.adc !== "1111" && onMilking) {
+      //   message = "Milking stopping...";
+      //   console.log(message);
+      //   onMilking = false;
+      // } else if (jsonData.adc == "1111" && onMilking) {
+      // }
+      // dataLine.rg_br = getRG(dataLine.r_br, dataLine.g_br);
+      // dataLine.rg_bl = getRG(dataLine.r_bl, dataLine.g_bl);
+      // dataLine.rg_fr = getRG(dataLine.r_fr, dataLine.g_fr);
+      // dataLine.rg_fl = getRG(dataLine.r_fl, dataLine.g_fl);
+      // dataLine.msg = getMsgFrom([
+      //   dataLine.rg_br,
+      //   dataLine.rg_bl,
+      //   dataLine.rg_fr,
+      //   dataLine.rg_fl,
+      // ]);
+      //await fs.appendFile(`${milkingName}.csv`, csvLine + '\n');
+      // await storage.insertData(dataLine);
+      console.log("Inserted data: ", dataLine);
+    } catch (err) {
+      console.error("Error writing to file", err);
+    }
   });
 
-  ws.on('close', function () {
-    console.log("Closed!")
-    clients.delete(ws);
+  ws.on("close", () => {
+    console.log("Client closed!");
+    if (isMilkingActive) {
+      console.log("Milking end.");
+      isMilkingActive = false;
+      milkingName = "";
+    }
   });
 });
 
-console.log(`WebSocket server is running on port ${PORT}`);
+function createMilkingFileName() {
+  const now = new Date();
+  return `${now.getDate()}_${
+    now.getMonth() + 1
+  }_${now.getFullYear()}_${now.getHours()}_${now.getMinutes()}`;
+}
+
+function getMsgFrom(rgArr) {
+  let msg = "";
+  let arr = [" BR", " BL", " FR", " FL"];
+  let isBlood = false;
+  for (idx in rgArr) {
+    if (rgArr[idx] > 1.2) {
+      isBlood = true;
+      msg += arr[idx];
+    }
+  }
+  if (isBlood) {
+    msg = "Blood at" + msg;
+  }
+  return msg;
+}
+
+function getRG(r, g) {
+  if (r != EMPTY_VALUE && g != EMPTY_VALUE) {
+    return parseFloat((r / g).toFixed(2));
+  } else {
+    return EMPTY_VALUE;
+  }
+}
+
+async function main() {
+  try {
+    console.log(`WebSocket server is running on port ${PORT}`);
+    // await storage.open();
+  } catch (err) {}
+}
+main();
---
+++ milkingStorage.js
@@ -0,0 +1,138 @@
+const { Pool } = require("pg");
+const debug = require("debug")("milking-storage:db");
+
+const MILKING_DATA_DDL = `
+  CREATE TABLE IF NOT EXISTS sensor_data (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    date DATE,
+    time TIME,
+    adc0 INT,
+    adc1 INT,
+    adc2 INT,
+    adc3 INT,
+    r_br INT,
+    g_br INT,
+    b_br INT,
+    ir1_br INT,
+    ir2_br INT,
+    r_bl INT,
+    g_bl INT,
+    b_bl INT,
+    ir1_bl INT,
+    ir2_bl INT,
+    r_fr INT,
+    g_fr INT,
+    b_fr INT,
+    ir1_fr INT,
+    ir2_fr INT,
+    r_fl INT,
+    g_fl INT,
+    b_fl INT,
+    ir1_fl INT,
+    ir2_fl INT
+  )
+`;
+
+const MILKING_DATA_INSERT = `
+    INSERT INTO sensor_data (
+      name, date, time, adc0, adc1, adc2, adc3,
+      r_br, g_br, b_br, ir1_br, ir2_br,
+      r_bl, g_bl, b_bl, ir1_bl, ir2_bl,
+      r_fr, g_fr, b_fr, ir1_fr, ir2_fr,
+      r_fl, g_fl, b_fl, ir1_fl, ir2_fl      
+    ) VALUES ($1, $2, $3, $4, $5, $6,
+        $7, $8, $9, $10, $11,
+        $12, $13, $14, $15, $16,
+        $17, $18, $19, $20, $21,
+        $22, $23, $24, $25, $26,
+        $27
+    );
+`;
+
+class MilkingStorage {
+  constructor() {
+    this.pool = new Pool({
+      connectionString: process.env.DATABASE_URL || 'postgresql://postgres:admin@localhost/sensor_db',
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
+    });
+  }
+
+  async open() {
+    const client = await this.pool.connect();
+    try {
+      await client.query(MILKING_DATA_DDL);
+      console.log("Sensor data database initialized.");
+    } catch (err){
+      debug(err);
+    } 
+    finally {
+      client.release();
+    }
+  }
+
+  async insertData(data) {
+    debug('Inserting sensor data', data);
+    await this.pool.query(MILKING_DATA_INSERT, Object.values(data));
+  }
+}
+
+
+const MILKING_DATA_DDL_OLD = `
+  CREATE TABLE IF NOT EXISTS sensor_data (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    date DATE,
+    time TIME,
+    adc0 INT,
+    adc1 INT,
+    adc2 INT,
+    adc3 INT,
+    r_br INT,
+    g_br INT,
+    b_br INT,
+    ir1_br INT,
+    ir2_br INT,
+    r_bl INT,
+    g_bl INT,
+    b_bl INT,
+    ir1_bl INT,
+    ir2_bl INT,
+    r_fr INT,
+    g_fr INT,
+    b_fr INT,
+    ir1_fr INT,
+    ir2_fr INT,
+    r_fl INT,
+    g_fl INT,
+    b_fl INT,
+    ir1_fl INT,
+    ir2_fl INT,
+    rg_br FLOAT,
+    rg_bl FLOAT,
+    rg_fr FLOAT,
+    rg_fl FLOAT,
+    msg TEXT
+  )
+`;
+
+const MILKING_DATA_INSERT_OLD = `
+    INSERT INTO sensor_data (
+      name, date, time, adc0, adc1, adc2, adc3,
+      r_br, g_br, b_br, ir1_br, ir2_br,
+      r_bl, g_bl, b_bl, ir1_bl, ir2_bl,
+      r_fr, g_fr, b_fr, ir1_fr, ir2_fr,
+      r_fl, g_fl, b_fl, ir1_fl, ir2_fl,
+      rg_br, rg_bl, rg_fr, rg_fl, msg
+    ) VALUES ($1, $2, $3, $4, $5, $6,
+        $7, $8, $9, $10, $11,
+        $12, $13, $14, $15, $16,
+        $17, $18, $19, $20, $21,
+        $22, $23, $24, $25, $26,
+        $27, $28, $29, $30, $31, $32
+    );
+`;
+
+module.exports = MilkingStorage;
